0f0240df76da371fa1836055309b5d2b4b259f52
Abssssss1753 <77192440+Abssssss1753@users.noreply.github.com>
2021-01-30 13:04:44 +0530

    Add files via upload

---
+++ Class 33.py
@@ -0,0 +1,34 @@
+# ############################ global keyword #############
+# a=10 # global variable
+# def function1():
+#     a=20 # loval variable
+#     print(a)
+# def function2():
+#     print(a)
+# function1()
+# function2()
+#
+# #######  after declare the global variable in the function1() #####
+#
+# a=10
+# def function1():
+#     global a
+#     a=20     # global variable
+#     print(a)
+# def function2():
+#     print(a)
+#
+# function1()
+# function2()
+#
+# ################# without declareing outside the global varible  #######
+#
+# def function1():
+#     global a
+#     a=20
+#     print(a)
+# def function2():
+#     print(a)
+#
+# function1()
+# function2()
